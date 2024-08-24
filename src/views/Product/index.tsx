c372d127a2a334f169fefd1d53aa67ddad2188de
Terry <hang0612@gmail.com>
2024-08-24 17:19:07 +0800

    feat: product page updated

--- src/views/Product/index.tsx
+++ src/views/Product/index.tsx
@@ -1,73 +1,126 @@
 import React, { useState } from 'react';
-import { Row, Col, Button, Card } from 'antd';
+import {
+  Row,
+  Col,
+  Button,
+  Card,
+  Layout,
+  Menu,
+  Checkbox,
+  Slider,
+  Flex,
+} from 'antd';
 import { FaSearch, FaBars } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import styles from './index.module.scss';
 
+import Item1 from '../../assets/item1.jpg';
+
 const { Meta } = Card;
+const { Sider } = Layout;
 
 const Product: React.FC = () => {
+  const [priceRange, setPriceRange] = useState([0, 100]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const handlePriceRangeChange = (values: [number, number]) => {
+    setPriceRange(values);
+  };
+
+  const handleCategoryChange = (categories: string[]) => {
+    setSelectedCategories(categories);
+  };
+
   return (
-    <div>
-      <Row gutter={[16, 16]}>
-        <Col xs={24} sm={12} md={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+    // <Flex justify="space-between">
+    <Layout style={{ paddingTop: '5px' }}>
+      <Sider width={300} className="site-layout-background">
+        <Menu
+          mode="vertical"
+          style={{
+            width: 300,
+            height: '100%',
+            borderRight: 0,
+            padding: '24px',
+          }}
+        >
+          <Menu.Item style={{ height: '200px' }}>
+            <h3>Price Range</h3>
+            <Slider
+              range
+              min={0}
+              max={100}
+              value={priceRange}
+              onChange={handlePriceRangeChange}
+            />
+            <p>
+              ${priceRange[0]} - ${priceRange[1]}
+            </p>
+          </Menu.Item>
+          <Menu.Item style={{ height: '200px' }}>
+            <h3>Categories</h3>
+            <Checkbox.Group
+              options={['Category A', 'Category B', 'Category C', 'Category D']}
+              value={selectedCategories}
+              onChange={handleCategoryChange}
+            />
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <div style={{ paddingLeft: '5px' }}>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
               />
-            }
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} md={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
               />
-            }
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} md={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
               />
-            }
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} md={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
               />
-            }
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
-        </Col>
-      </Row>
-    </div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </Layout>
+    // </Flex>
   );
 };
 
--- src/views/Welcome/index.tsx
+++ src/views/Welcome/index.tsx
@@ -8,13 +8,56 @@ import NavigationHeader from '../../component/NavigationHeader';
 const { Meta } = Card;
 const { Content, Footer } = Layout;
 const { Title } = Typography;
+import Item1 from '../../assets/item1.jpg';
 
 const Welcome: React.FC = () => {
   return (
-    <div className="title">
-      <Title>Welcome</Title>
-      <Title level={5}>arngen</Title>
-      <Button>Shopping</Button>
+    <div>
+      <div className="title">
+        <Title>Welcome</Title>
+        <Title level={5}>arngen</Title>
+        <Button>Shopping</Button>
+      </div>
+      <div style={{ paddingLeft: '5px' }}>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src={Item1} />}
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
